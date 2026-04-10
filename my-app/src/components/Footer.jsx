d3b4a7f21c6ad92ed1653c29512134e7584baa39
Surabhi Prajapati <127585333+Surabhi-7@users.noreply.github.com>
2026-04-10 16:51:57 +0530

    Update Footer.jsx

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -66,7 +66,7 @@ const Footer = () => {
   <FaInstagram className="w-5 h-5 text-white" />
 </a>
 
-              <a href="#" className="w-10 h-10 bg-gray-800 hover:bg-red-600 rounded-lg flex items-center justify-center transition-all duration-300 hover:scale-110">
+              <a href="https://www.linkedin.com/in/techbro31" className="w-10 h-10 bg-gray-800 hover:bg-red-600 rounded-lg flex items-center justify-center transition-all duration-300 hover:scale-110">
                 <FaLinkedinIn className="w-5 h-5 text-white" />
               </a>
 
